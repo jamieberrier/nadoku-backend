34411721bac7ca068e185825569cf0800e9e8f14
jamieberrier <berrier.jamie@gmail.com>
2020-10-05 13:55:54 -0700

    creates namespaced routes for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[create edit update destory]
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
