321903204216365af958a3545c630cbcaddf93ab
jamieberrier <berrier.jamie@gmail.com>
2020-10-06 15:13:52 -0700

    creates route for logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: %i[create edit update destory]
       post '/login' => 'sessions#create', as: 'login'
-      get '/get_current_user' => 'sessions#get_current_user'
+      get '/get_current_user' => 'sessions#render_current_user'
+      delete '/logout' => 'sessions#destroy', as: 'logout'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
