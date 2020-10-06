5ae8ae75ecdbb51761a047a7dfd7abdaaf204ac6
jamieberrier <berrier.jamie@gmail.com>
2020-10-06 13:11:22 -0700

    adds routes for login and get_current_user

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[create edit update destory]
+      post '/login' => 'sessions#create', as: 'login'
+      get '/get_current_user' => 'sessions#get_current_user'
     end
   end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
