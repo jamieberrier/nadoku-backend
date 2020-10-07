ff8acc7a77d18da0763101c12be6691c687de7df
jamieberrier <berrier.jamie@gmail.com>
2020-10-07 09:34:58 -0700

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: %i[create edit update destory]
-      post '/login' => 'sessions#create', as: 'login'
+      post '/login' => 'sessions#create'
       get '/get_current_user' => 'sessions#render_current_user'
-      delete '/logout' => 'sessions#destroy', as: 'logout'
+      delete '/logout' => 'sessions#destroy'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
