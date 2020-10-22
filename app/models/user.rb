529f7339061b725522ef51421371d5b6d1c5e776
jamieberrier <berrier.jamie@gmail.com>
2020-10-22 11:54:47 -0700

    adds validation for username presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, confirmation: { case_sensitive: false }
+  validates :username, presence: true
 end
