8c07fda0fe09388d85ea39dacb194db052a1d148
jamieberrier <berrier.jamie@gmail.com>
2020-10-22 13:32:45 -0700

    adds status to render in render_current_user and adds custom validations to create

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,24 +2,24 @@ class Api::V1::SessionsController < ApplicationController
   before_action :set_user, only: %i[create]
 
   def create
+    password = params[:session][:password]
     # SafeNavigation
     ## This cop transforms usages of a method call safeguarded by a non `nil` check for the variable whose method is being called to safe navigation (`&.`).
-    if @user&.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
-      render json: UserSerializer.new(@user)
-    else
-      render json: {
-        error: 'Invalid Credentials'
-      }
+    if @user&.authenticate(password)
+      log_in_user
+    elsif @user
+      check_if_password_blank(password)
+    elsif @user.nil?
+      check_if_email_blank(params[:session][:email], password)
     end
   end
 
   def render_current_user
     if logged_in?
-      render json: UserSerializer.new(current_user)
+      render json: UserSerializer.new(current_user), status: :ok
     else
-      @user = User.new(email: '', password: '')
-      render json: UserSerializer.new(@user)
+      @user = User.new(email: '', username: '', password: '')
+      render json: UserSerializer.new(@user), status: :ok
     end
   end
 
@@ -35,5 +35,32 @@ class Api::V1::SessionsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find_by(email: params[:session][:email])
+  end
+
+  def log_in_user
+    session[:user_id] = @user.id
+    render json: UserSerializer.new(@user), status: :ok
+  end
+
+  def check_if_password_blank(password)
+    if password.blank?
+      render_with_error('Please Enter Password')
+    else
+      render_with_error('Invalid Password')
+    end
+  end
+
+  def check_if_email_blank(email, password)
+    if email.blank? && password.blank?
+      render_with_error('Please Enter Email and Password')
+    elsif email.blank?
+      render_with_error('Please Enter Email')
+    else
+      render_with_error('Invalid Email')
+    end
+  end
+
+  def render_with_error(message)
+    render json: { error: message }, status: 422
   end
 end
